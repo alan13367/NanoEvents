b42c8556e3cb3582b3fa55de2c3e64a5f11780cd
Alvaro Alejandro Feher Argullós <alvaro.feher@students.salle.url.edu>
2022-04-15 17:28:58 +0200

    login started

---
+++ app/src/main/java/com/example/nanoevents/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.nanoevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.signInBtn);
+        button.setPaintFlags(button.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    }
+}
